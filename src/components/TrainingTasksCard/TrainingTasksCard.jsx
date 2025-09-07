42a27794b07de18a2dea85f59117a7f96eea6021
sagderovkamalit <sagderovkamal.it@gmail.com>
2025-09-07 16:32:54 +0300

    add

--- src/components/TrainingTasksCard/TrainingTasksCard.jsx
+++ src/components/TrainingTasksCard/TrainingTasksCard.jsx
@@ -9,104 +9,110 @@ import Button from "../Button/Button";
 import { trainingTasksData } from "../../data/trainingTasks";
 
 import arrowLeft from "../../assets/images/trainingTasks/arrow-left.svg";
+import arrowLeftTablet from "../../assets/images/trainingTasks/arrow-left-tablet.svg";
+import arrowLeftMobile from "../../assets/images/trainingTasks/arrow-left-mobile.svg";
 import arrowRight from "../../assets/images/trainingTasks/arrow-right.svg";
+import arrowRightTablet from "../../assets/images/trainingTasks/arrow-right-tablet.svg";
 import arrowRightMobile from "../../assets/images/trainingTasks/arrow-right-mobile.svg";
 
 
-
-
-import arrowLeftTablet from "../../assets/images/trainingTasks/arrow-left-tablet.svg";
-import arrowRightTablet from "../../assets/images/trainingTasks/arrow-right-tablet.svg";
-
-
-function TrainingTasksCard({ id, title, paragraphs, details, highlights, skills , slogan, sloganTitle, sloganParagraph, sloganButton}) {
+function TrainingTasksCard({
+  id,
+  title,
+  paragraphs,
+  details,
+  highlights,
+  skills,
+  slogan,
+  sloganTitle,
+  sloganParagraph,
+  sloganButton,
+}) {
   const cardClass =
-    id === "ai-engineer" ? TrainingCardStyles.cardAI :
-    id === "digital-managers" ? TrainingCardStyles.cardDigital :
-    TrainingCardStyles.card;
+    id === "ai-engineer"
+      ? TrainingCardStyles.cardAI
+      : id === "digital-managers"
+      ? TrainingCardStyles.cardDigital
+      : TrainingCardStyles.card;
 
   return (
     <article className={`${TrainingCardStyles.card} ${cardClass}`}>
-       <header className={TrainingCardStyles.header}>
-          <span className={TrainingCardStyles.badge}>{title}</span>
-        </header>
+      <header className={TrainingCardStyles.header}>
+        <span className={TrainingCardStyles.badge}>{title}</span>
+      </header>
 
-        <div className={TrainingCardStyles.trainingContainer} >
-
-       <div className={TrainingCardStyles.trainingContentImg} >
-
-      <div className={TrainingCardStyles.trainingContent}>
-      
-        <div className={TrainingCardStyles.body}>
-          {paragraphs.map(item => (
-            <Paragraph key={item.id} className={TrainingCardStyles.trainingParagraph}>
-              {item.text}
-            </Paragraph>
-          ))}
-        </div>
-        <div className={TrainingCardStyles.details}>
-          {details.map(detail => (
-            <div key={detail.id} className={TrainingCardStyles.detailGroup}>
-              {detail.texts.map((line, i) => (
-                <p key={i} className={TrainingCardStyles.detailText}>{line}</p>
+      <div className={TrainingCardStyles.trainingContainer}>
+        <div className={TrainingCardStyles.trainingContentImg}>
+          <div className={TrainingCardStyles.trainingContent}>
+            <div className={TrainingCardStyles.body}>
+              {paragraphs.map((item) => (
+                <Paragraph
+                  key={item.id}
+                  className={TrainingCardStyles.trainingParagraph}
+                >
+                  {item.text}
+                </Paragraph>
               ))}
             </div>
-          ))}
-        </div>
-        {highlights.length > 0 && (
-          <div className={TrainingCardStyles.highlights}>
-            {highlights.map(h => <span key={h.id}>{h.text}</span>)}
+            <div className={TrainingCardStyles.details}>
+              {details.map((detail) => (
+                <div key={detail.id} className={TrainingCardStyles.detailGroup}>
+                  {detail.texts.map((line, i) => (
+                    <p key={i} className={TrainingCardStyles.detailText}>
+                      {line}
+                    </p>
+                  ))}
+                </div>
+              ))}
+            </div>
+            {highlights.length > 0 && (
+              <div className={TrainingCardStyles.highlights}>
+                {highlights.map((h) => (
+                  <span key={h.id}>{h.text}</span>
+                ))}
+              </div>
+            )}
+            <div className={TrainingCardStyles.skills}>
+              {skills.map((skill) => (
+                <span key={skill.id} className={TrainingCardStyles.skill}>
+                  {skill.text}
+                </span>
+              ))}
+            </div>
           </div>
-        )}
-        <div className={TrainingCardStyles.skills}>
-          {skills.map(skill => (
-            <span key={skill.id} className={TrainingCardStyles.skill}>{skill.text}</span>
-          ))}
         </div>
-      </div>
-       </div>
 
         <div className={TrainingCardStyles.sloganBlock}>
+          <h3 className={TrainingCardStyles.slogan}>{slogan}</h3>
 
-        
-        <h3 className={TrainingCardStyles.slogan}>{slogan}</h3>
-
-
-        <div className={TrainingCardStyles.sloganBlockText} >
-
-        <h4 className={TrainingCardStyles.sloganTitle}>{sloganTitle}</h4>
-        <Paragraph className={TrainingCardStyles.sloganParagraph}>{sloganParagraph}</Paragraph>
+          <div className={TrainingCardStyles.sloganBlockText}>
+            <h4 className={TrainingCardStyles.sloganTitle}>{sloganTitle}</h4>
+            <Paragraph className={TrainingCardStyles.sloganParagraph}>
+              {sloganParagraph}
+            </Paragraph>
           </div>
+        </div>
 
+        <div className={TrainingCardStyles.buttonContainer}>
+          <picture className={TrainingCardStyles.buttonContainerArrow}>
+            <source media="(max-width: 490px)" srcSet={arrowLeftMobile} />
+            <source media="(max-width: 1024px)" srcSet={arrowLeftTablet} />
+            <img src={arrowLeft} alt="arrowLeft" />
+          </picture>
+          <Button
+            className={TrainingCardStyles.sloganButton}
+            text={sloganButton}
+          ></Button>
+
+          <picture className={TrainingCardStyles.buttonContainerArrow}>
+            <source media="(max-width: 490px)" srcSet={arrowRightMobile} />
+            <source media="(max-width: 1024px)" srcSet={arrowRightTablet} />
+            <img src={arrowRight} alt="arrowRight" />
+          </picture>
+        </div>
       </div>
-
-      
-      
-
-          <div className={TrainingCardStyles.buttonContainer} >
-
-            <img  className={TrainingCardStyles.buttonContainerArrow} src={arrowLeft} alt="arrowLeft" />
-
-            <Button className={TrainingCardStyles.sloganButton} text={sloganButton} >
- 
-
-             
-             
-            </Button>
-
-             <img  className={TrainingCardStyles.buttonContainerArrow} src={arrowRight} alt="arrowRight" />
-
-
-   </div>
-     </div>
-   
-     
-
-      
-
     </article>
   );
 }
 
 export default TrainingTasksCard;
-
